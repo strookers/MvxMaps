af3ba7d26ebfe314089ca4948e6b4e7083513e0a
strookers <michael_k@mail.dk>
2016-11-05 18:01:18 +0100

    Tilføjet en service og gjort klar til at lave en rute

---
+++ MvxMaps.Core/Services/GoogleMapService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvxMaps.Core.ViewModels;
+
+namespace MvxMaps.Core.Services
+{
+    public class GoogleMapService : IGoogleMapService
+    {
+        public List<Location> FnDecodePolylinePoints(string encodedPoints)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FnDrawPath(string strSource, string strDestination)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> FnHttpRequest(string strUri)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> FnLocationToLatLng()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MvxMaps.Core/Services/IGoogleMapService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvxMaps.Core.ViewModels;
+
+namespace MvxMaps.Core.Services
+{
+    public interface IGoogleMapService
+    {
+        //burde returnere en string bestående af PolyLinePoints fra google til at tegne ruten
+        void FnDrawPath(string strSource, string strDestination);
+
+        //Location er en viewModel fra Core. Er ikke sikker på at om den skal være der!
+        List<Location> FnDecodePolylinePoints(string encodedPoints);
+
+        //Vil mene denne her skal returnere en string eller en Location/LatLng
+        Task<bool> FnLocationToLatLng();
+
+        //taget fra DrawMapPathXamarin. burde muligvis hænge sammen med FnLocationToLatLng
+        Task<string> FnHttpRequest(string strUri);
+    }
+}
